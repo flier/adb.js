5174b18fa9a4d29c3d306ab945d8cbf2653566d7
Flier Lu <flier.lu@gmail.com>
2012-07-09 13:36:40 +0800

    take snapshot with the framebuffer command

--- adb.js
+++ adb.js
@@ -1,5 +1,6 @@
 var console = require('console');
 var net = require('net');
+var events = require('events');
 var util = require('util');
 
 var DebugBridge = exports.DebugBridge = function () {
@@ -11,10 +12,14 @@ var DebugBridge = exports.DebugBridge = function () {
     this.autoStartDaemon = true;
 };
 
+util.inherits(DebugBridge, events.EventEmitter);
+
 var DebugSession = function (adb, sock) {
     this.adb = adb;
     this.sock = sock;
 };
+
+util.inherits(DebugSession, events.EventEmitter);
 
 DebugBridge.DEFAULT_PORT = 5037;
 
@@ -65,13 +70,18 @@ DebugBridge.prototype.connect = function (callback /* (session: DebugSession) */
     var sock = net.connect({ port: DebugBridge.DEFAULT_PORT }, function () {
         console.log('client connected to %s:%d', sock.remoteAddress, sock.remotePort);
 
-        callback(new DebugSession(adb, sock));
+        sock.session = new DebugSession(adb, sock);
+        adb.emit('connected', sock.session);
+
+        callback(sock.session);
     });
 
     sock.setKeepAlive(true);
     sock.setNoDelay(true);
     sock.on('end', function () {
         console.log('client disconnected');
+
+        sock.session.emit('disconnected');
 
         sock.end();
     });
@@ -129,7 +139,34 @@ DebugSession.prototype.sendData = function (data) {
     this.sock.write(buf);
 };
 
+DebugSession.prototype.parseCmdResult = function (data, callback /* (data: buffer) */) {
+    var code = data.slice(0, 4).toString();
+
+    if (code == 'OKAY') {
+        console.log('exec command succeeded');
+
+        this.parseData(data.slice(4), callback);
+    } else if (code == 'FAIL') {
+        console.log('exec command failed');
+
+        this.parseData(data.slice(4), function (msg) {
+            throw new Error(msg);
+        });
+    } else {
+        throw new Error('Unknown result code - ' + code);
+    }
+
+    return code;
+}
+
 DebugSession.prototype.parseData = function (data, callback /* (data: buffer) */) {
+    var session = this;
+
+    if (data.length == 0) {
+        callback(null);
+        return;
+    }
+
     var len = decodeNumber(data.slice(0, 4));
 
     console.log('found %d bytes data: %s', len, data.slice(4, 4+len));
@@ -140,38 +177,50 @@ DebugSession.prototype.parseData = function (data, callback /* (data: buffer) */
         callback(data.slice(4, 4+len));
 
         if (data.length > 4+len) {
-            adb.recvData(data.slice(4+len), callback);
+            session.parseData(data.slice(4+len), callback);
         }
     }
 };
 
-DebugSession.prototype.recvData = function (callback /* (data: buffer) */) {
-    var adb = this;
+DebugSession.prototype.waitCmdResult = function (callback /* (data: buffer) */) {
+    var session = this;
 
-    this.sock.on('data', function (data) {
-        console.log('recv %d bytes: %s', data.length, data);
+    this.sock.once('data', function (data) {
+        console.log('recv %d bytes data: %s', data.length, data);
 
-        var code = data.slice(0, 4).toString();
-
-        if (code == 'OKAY') {
-            console.log('exec command succeeded');
-
-            adb.parseData(data.slice(4), callback);
-        } else if (code == 'FAIL') {
-            console.log('exec command failed');
-
-            adb.parseData(data.slice(4), function (msg) {
-                throw new Error(msg);
-            });
-        } else {
-            adb.parseData(data, callback)
-        }
+        session.parseCmdResult(data, callback);
     });
 };
 
-DebugBridge.prototype.execCommand = function (cmd, callback /* (data: buffer) */) {
+DebugSession.prototype.recvData = function (callback /* (data: buffer) */) {
+    this.sock.on('data', callback);
+};
+
+DebugBridge.prototype.execCommand = function (cmd, callback /* (data: buffer) */, repeat) {
     this.connect(function (session) {
-        session.recvData(callback);
+        session.waitCmdResult(callback, repeat);
+        session.sendData(cmd);
+    });
+};
+
+DebugBridge.TRANSPORT_USB = 'host:transport-usb';
+DebugBridge.TRANSPORT_LOCAL = 'host:transport-local';
+DebugBridge.TRANSPORT_ANY = 'host:transport-any';
+
+DebugBridge.prototype.prepareTransport = function (sn_or_type, callback) {
+    this.connect(function (session) {
+        session.waitCmdResult(function (data) {
+            callback(session);
+        });
+
+        var cmd;
+
+        if (sn_or_type.indexOf('host:transport-') == 0) {
+            cmd = sn_or_type;
+        } else {
+            cmd = 'host:transport:' + sn_or_type;
+        }
+
         session.sendData(cmd);
     });
 };
@@ -210,10 +259,10 @@ DebugBridge.prototype.traceDevice = function (callback /* (devices: AndroidDevic
 
     this.execCommand('host:track-devices', function (data) {
         callback(parseDevices(adb, data));
-    });
+    }, true);
 };
 
-var AndroidDevice = exports.AndroidDevice = function (adb, id, type) {
+var AndroidDevice = function (adb, id, type) {
     this.adb = adb;
     this.id = id;
     this.type = type;
@@ -228,6 +277,92 @@ Object.defineProperty(AndroidDevice.prototype, 'isEmulator', {
         return this.id.indexOf("emulator-") == 0;
     }
 });
+
+AndroidDevice.prototype.takeSnapshot = function (callback /* (frame: Framebuffer) */) {
+    this.adb.prepareTransport(this.id, function (session) {
+        session.waitCmdResult(function (data) {
+            session.recvData(function (data) {
+                if (!session.frame) {
+                    session.frame = new AndroidFrame();
+                }
+
+                session.frame.parseData(data);
+
+                if (session.frame.isFinished) {
+                    var frame = session.frame;
+
+                    session.frame = null;
+
+                    callback(frame);
+                }
+            }, true);
+        });
+        session.sendData('framebuffer:');
+    });
+};
+
+var AndroidFrame = function () {
+};
+
+AndroidFrame.prototype.toString = function () {
+    return util.format('<frame %dx%d with %d bytes>', this.width, this.height, this.size);
+};
+
+Object.defineProperty(AndroidFrame.prototype, 'isFinished', {
+    get: function () {
+        return this.pixels && (this.size == this.pixels.length);
+    }
+});
+
+AndroidFrame.prototype.parseData = function (data) {
+    if (this.pixels) {
+        this.pixels = Buffer.concat([this.pixels, data], this.pixels.length + data.length);
+    } else {
+        this.pixels = data;
+    }
+
+    if (!this.size && this.pixels.length >= 16) {
+        var version = this.pixels.readUInt32LE(0);
+
+        if (version == 1) {
+            this.depth = this.pixels.readUInt32LE(4);
+            this.size = this.pixels.readUInt32LE(8);
+            this.width = this.pixels.readUInt32LE(12);
+            this.height = this.pixels.readUInt32LE(16);
+
+            // create default values for the rest. Format is 565
+            this.red_offset = this.pixels.readUInt32LE(20);
+            this.red_length = this.pixels.readUInt32LE(24);
+            this.green_offset = this.pixels.readUInt32LE(28);
+            this.green_length = this.pixels.readUInt32LE(32);
+            this.blue_offset = this.pixels.readUInt32LE(36);
+            this.blue_length = this.pixels.readUInt32LE(40);
+            this.alpha_offset = this.pixels.readUInt32LE(44);
+            this.alpha_length = this.pixels.readUInt32LE(48);
+
+            this.pixels = this.pixels.slice(13*4);
+        } else if (version == 16) {
+            this.depth = 16;
+            this.size = this.pixels.readUInt32LE(4);
+            this.width = this.pixels.readUInt32LE(8);
+            this.height = this.pixels.readUInt32LE(12);
+
+            // create default values for the rest. Format is 565
+            this.red_offset = 11;
+            this.red_length = 5;
+            this.green_offset = 5;
+            this.green_length = 6;
+            this.blue_offset = 0;
+            this.blue_length = 5;
+            this.alpha_offset = 0;
+            this.alpha_length = 0;
+
+            this.pixels = this.pixels.slice(16);
+        }
+
+        console.log('found a %dx%d@%d frame with %d bytes', this.width, this.height, this.depth, this.size);
+    }
+}
 
 var adb = new DebugBridge();
 
@@ -245,5 +380,8 @@ adb.traceDevice(function (devices) {
     for (var i=0; i<devices.length; i++) {
         var device = devices[i];
 
+        device.takeSnapshot(function (frame) {
+            console.log(frame.toString());
+        });
     }
 });
